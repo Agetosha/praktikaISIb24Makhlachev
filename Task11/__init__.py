35405da13b22dafc5ab87851cf138f47f28dba86
Agetosha <mahlachev44@gmail.com>
2025-09-16 23:24:02 +0800

    добавил 11 задание

---
+++ Task11/__init__.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("test2.jpg")
+result_image = image.copy()
+
+blurred_image = cv2.GaussianBlur(image, (11, 11), 0)
+hsv_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
+
+hsv_min = np.array((36, 25, 25), np.uint8)
+hsv_max = np.array((70, 255, 255), np.uint8)
+
+green_mask = cv2.inRange(hsv_image, hsv_min, hsv_max)
+contours, hierarchy = cv2.findContours(green_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for i, contour in enumerate(contours):
+    if cv2.contourArea(contour) < 100:
+        continue
+    
+    x, y, w, h = cv2.boundingRect(contour)
+    center_x = int(x + w/2)
+    center_y = int(y + h/2)
+    
+    cv2.circle(result_image, (center_x, center_y), 10, (0, 0, 255), -1)
+    cv2.drawContours(result_image, [contour], -1, (255, 0, 0), 2)
+    
+    print(f"Объект {i+1}: центр в точке ({center_x}, {center_y}), размер: {w}x{h}")
+
+cv2.imshow("Оригинальное изображение", image)
+cv2.imshow("Зеленая маска", green_mask)
+cv2.imshow("Результат с отмеченными центрами", result_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
